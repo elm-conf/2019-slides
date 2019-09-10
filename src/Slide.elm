3b97b52528bd3b4257a55b1945d907ac4c6265bd
Brian Hicks <brian@brianthicks.com>
2019-09-10 16:25:59 -0500

    make paragraph styles

--- src/Slide.elm
+++ src/Slide.elm
@@ -15,7 +15,7 @@ view slide =
         CenteredInfo { title, text } ->
             Html.section []
                 [ heading title
-                , Html.p [] [ Html.text text ]
+                , paragraph [ Html.text text ]
                 ]
 
 
@@ -23,8 +23,20 @@ heading : String -> Html msg
 heading text =
     Html.h1
         [ css
-            [ Css.fontSize (rem 10)
+            [ Css.fontSize (Css.vh 12)
             , Css.fontFamilies [ "Vollkorn" ]
+            , Css.color (Css.hex "FF5F6D")
             ]
         ]
         [ Html.text text ]
+
+
+paragraph : List (Html msg) -> Html msg
+paragraph =
+    Html.p
+        [ css
+            [ Css.fontFamilies [ "Work Sans" ]
+            , Css.fontSize (Css.vh 5)
+            , Css.color (Css.hex "444")
+            ]
+        ]
