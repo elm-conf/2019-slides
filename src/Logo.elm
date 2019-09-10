1a1761189734454e8ad8e62a176624fde190d7ea
Brian Hicks <brian@brianthicks.com>
2019-09-10 16:44:10 -0500

    make logo styleable

--- src/Logo.elm
+++ src/Logo.elm
@@ -1,17 +1,20 @@
 module Logo exposing (logo)
 
+import Css exposing (Style)
 import Html.Styled as Html exposing (Html)
-import Svg exposing (..)
-import Svg.Attributes exposing (..)
+import Svg.Styled as Svg exposing (..)
+import Svg.Styled.Attributes exposing (..)
 
 
-logo : Html msg
-logo =
-    Html.fromUnstyled <|
-        svg [ viewBox "0 0 324 324" ]
-            [ Svg.path
-                [ d "M162 153l70-70H92zM9 0l70.265 70H232L161.734 0zm238.485 85l76.367 76.368-76.367 76.367-76.368-76.367zM324 144V0H180zm-171 17.999L0 9v306zM256 247l68 68V179zm-94-76L9 324h306z"
-                , fill "#FF5F6D"
-                ]
-                []
+logo : List Style -> Html msg
+logo styles =
+    svg
+        [ viewBox "0 0 324 324"
+        , css styles
+        ]
+        [ Svg.path
+            [ d "M162 153l70-70H92zM9 0l70.265 70H232L161.734 0zm238.485 85l76.367 76.368-76.367 76.367-76.368-76.367zM324 144V0H180zm-171 17.999L0 9v306zM256 247l68 68V179zm-94-76L9 324h306z"
+            , fill "#FF5F6D"
             ]
+            []
+        ]
