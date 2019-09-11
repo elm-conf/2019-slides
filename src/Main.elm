3fe55837ea12cd87680cff65668d354826f173f6
Brian Hicks <brian@brianthicks.com>
2019-09-11 12:37:45 -0500

    add slide about lunch

--- src/Main.elm
+++ src/Main.elm
@@ -92,6 +92,10 @@ init msg =
             , text = "HubTran is a fast-growing technology startup focused on automating work in the logistics industry. Through a combination of a powerful browser based interface and machine learning, we are able to reduce our customer's workload by 4x or more. HubTran believes that Elm is the future of the front end. Our front end is already more than 60% Elm and we're looking for engineers to join our 15 person remote friendly engineering team."
             }
         , Slide.Info
+            { title = "Lunch"
+            , text = "Shuttles will run between 20th Street and downtown restaurant areas. Food trucks at Union Station are only available for PWLConf attendees."
+            }
+        , Slide.Info
             { title = "Joel Clermont"
             , text = "..."
             }
