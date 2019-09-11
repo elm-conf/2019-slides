50e44361f3585a9ede59a3ee263574de438a0739
Brian Hicks <brian@brianthicks.com>
2019-09-11 12:33:42 -0500

    add Joel WIP slide

--- src/Main.elm
+++ src/Main.elm
@@ -91,6 +91,8 @@ init msg =
             , title = "HubTran"
             , text = "HubTran is a fast-growing technology startup focused on automating work in the logistics industry. Through a combination of a powerful browser based interface and machine learning, we are able to reduce our customer's workload by 4x or more. HubTran believes that Elm is the future of the front end. Our front end is already more than 60% Elm and we're looking for engineers to join our 15 person remote friendly engineering team."
             }
+        , Slide.Info
+            { title = "Joel Clermont"
             , text = "..."
             }
         ]
