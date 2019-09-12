be8a21a02ccc5a6600ba9d3af528b895f683e041
Brian Hicks <brian@brianthicks.com>
2019-09-11 22:23:24 -0500

    simplify nowplaying to use the python script

--- src/NowPlaying.elm
+++ src/NowPlaying.elm
@@ -15,11 +15,8 @@ get : String -> (Result Http.Error Song -> msg) -> Cmd msg
 get token msg =
     Http.request
         { method = "GET"
-        , headers =
-            [ Http.header "Accept" "application/json"
-            , Http.header "Authorization" ("Bearer " ++ token)
-            ]
-        , url = "https://api.spotify.com/v1/me/player/currently-playing"
+        , headers = []
+        , url = "http://localhost:8000"
         , body = Http.emptyBody
         , expect = expectSong msg
         , timeout = Just 5000
@@ -31,7 +28,7 @@ expectSong : (Result Http.Error Song -> msg) -> Http.Expect msg
 expectSong msg =
     Http.expectJson msg
         (Decode.map3 Song
-            (Decode.at [ "item", "name" ] Decode.string)
-            (Decode.at [ "item", "album", "artists" ] (Decode.index 0 (Decode.field "name" Decode.string)))
-            (Decode.at [ "item", "album", "images" ] (Decode.index 1 (Decode.field "url" Decode.string)))
+            (Decode.index 0 Decode.string)
+            (Decode.index 1 Decode.string)
+            (Decode.index 2 Decode.string)
         )
